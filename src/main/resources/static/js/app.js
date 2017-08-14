629bdd5bec93e14fcf5e0b04db29a63c360ebf1e
Max_well <Maxouwell@gmail.com>
2017-08-14 11:08:47 +0200

    Ajout de linkify Ajout de l'adresse du repo github

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -116,7 +116,7 @@ function createMessage(message, trClass) {
 		+ "] " 
 		+ message.username 
 		+ " > " 
-		+ message.content 
+		+ linkify(message.content)
 		+ "</td></tr>";
 }
 
@@ -128,3 +128,10 @@ $(function () {
     $( "#loadMoreBtn" ).click(function() { requestMessagesList(); });
     connect();
 });
+
+/* See https://github.com/jmrware/LinkifyURL */
+function linkify(text) {
+    var url_pattern = /(\()((?:ht|f)tps?:\/\/[a-z0-9\-._~!$&'()*+,;=:\/?#[\]@%]+)(\))|(\[)((?:ht|f)tps?:\/\/[a-z0-9\-._~!$&'()*+,;=:\/?#[\]@%]+)(\])|(\{)((?:ht|f)tps?:\/\/[a-z0-9\-._~!$&'()*+,;=:\/?#[\]@%]+)(\})|(<|&(?:lt|#60|#x3c);)((?:ht|f)tps?:\/\/[a-z0-9\-._~!$&'()*+,;=:\/?#[\]@%]+)(>|&(?:gt|#62|#x3e);)|((?:^|[^=\s'"\]])\s*['"]?|[^=\s]\s+)(\b(?:ht|f)tps?:\/\/[a-z0-9\-._~!$'()*+,;=:\/?#[\]@%]+(?:(?!&(?:gt|#0*62|#x0*3e);|&(?:amp|apos|quot|#0*3[49]|#x0*2[27]);[.!&',:?;]?(?:[^a-z0-9\-._~!$&'()*+,;=:\/?#[\]@%]|$))&[a-z0-9\-._~!$'()*+,;=:\/?#[\]@%]*)*[a-z0-9\-_~$()*+=\/#[\]@%])/img;
+    var url_replace = '$1$4$7$10$13<a href="$2$5$8$11$14">$2$5$8$11$14</a>$3$6$9$12';
+    return text.replace(url_pattern, url_replace);
+}
